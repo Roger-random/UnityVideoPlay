af8f47da61f27ce818ae63b4aebc16bf44b97042
Roger Cheng <roger.random@outlook.com>
2021-01-30 00:21:14 -0800

    Concept works

---
+++ MatrixTest/Assets/PlayButton.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+public class PlayButton : MonoBehaviour
+{
+    public PlaybackLogic playbackLogic;
+    public InputActionAsset playerControls;
+    public InputAction fireAction;
+
+    private Collider buttonCollider;
+
+    private void Awake()
+    {
+        InputActionMap inputActionMap = playerControls.FindActionMap("Player");
+
+        fireAction = inputActionMap.FindAction("Fire");
+        fireAction.performed += FireAction_performed;
+
+        buttonCollider = GetComponent<Collider>();
+    }
+
+    private void FireAction_performed(InputAction.CallbackContext obj)
+    {
+        Vector2Control mousev2c = Mouse.current.position;
+        Vector3 mousePosition = new Vector3(mousev2c.x.ReadValue(), mousev2c.y.ReadValue(), 0);
+        Ray mouseRay = Camera.main.ScreenPointToRay(mousePosition);
+        RaycastHit hit;
+
+        if(buttonCollider.Raycast(mouseRay, out hit, 100f))
+        {
+            playbackLogic.PlayRandom();
+        }
+    }
+
+    private void OnEnable()
+    {
+        fireAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        fireAction.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MatrixTest/Assets/PlaybackLogic.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlaybackLogic : MonoBehaviour
+{
+    public VideoClip idleClip;
+    public VideoClip[] videoClips;
+
+    private VideoPlayer videoPlayer;
+    private bool playingIdle;
+
+    private void Awake()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayIdle();
+    }
+    private void PlayIdle()
+    {
+        playingIdle = true;
+        videoPlayer.playbackSpeed = 0.1f;
+        videoPlayer.isLooping = true;
+        videoPlayer.clip = idleClip;
+        videoPlayer.SetDirectAudioVolume(0, 0f);
+        videoPlayer.loopPointReached -= VideoPlayer_loopPointReached;
+        videoPlayer.Play();
+    }
+
+    public void PlayRandom()
+    {
+        if (playingIdle)
+        {
+            int videoIndex = Mathf.CeilToInt(Random.value * videoClips.Length - 1);
+            playingIdle = false;
+            videoPlayer.playbackSpeed = 1.0f;
+            videoPlayer.isLooping = false;
+            videoPlayer.clip = videoClips[videoIndex];
+            videoPlayer.SetDirectAudioVolume(0, 1f);
+            videoPlayer.loopPointReached += VideoPlayer_loopPointReached;
+            videoPlayer.Play();
+        }
+    }
+
+    private void VideoPlayer_loopPointReached(VideoPlayer source)
+    {
+        PlayIdle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
